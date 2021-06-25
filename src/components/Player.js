91321238241bd7e0fbd70bd3b33104bb42a14f73
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-06-25 15:30:11 +0300

    font icons

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlay , faAngleLeft , faAngleRight} from '@fortawesome/free-solid-svg-icons'
 
 const Player = () => {
     return (
@@ -8,7 +10,11 @@ const Player = () => {
     <input type="range"></input>
     <p> End time</p>
 </div>
-<div className="play-controll"></div>
+<div className="play-controll">
+<FontAwesomeIcon className="skip-back" icon={faAngleLeft}/>
+<FontAwesomeIcon className="play" icon={faPlay}/>
+<FontAwesomeIcon className="skip-forward" icon={faAngleRight}/>
+</div>
         </div>
     )
 }
