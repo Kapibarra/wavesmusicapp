c8b4ae9a68c96a2843721ff696cf9617b6ae60f5
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-06-27 14:56:30 +0300

    fix

--- src/util.js
+++ src/util.js
@@ -12,23 +12,15 @@ function chillHop() {
       active: true,
     },
     {
-      name: "Higher",
-      cover: "https://chillhop.com/wp-content/uploads/2021/05/2473c60e471fe9b40e246bf7711c87d3d6ea69a7-1024x1024.jpg",
-      artist: "Misha, Jussi Halme",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=15088",
-      color: ["#EA7A76", "#2E385B"],
+      name: "travelbag",
+      cover: "https://chillhop.com/wp-content/uploads/2021/05/1245c0271290a3196328c0cf4aaa910cd873dfe7-1024x1024.jpg",
+      artist: "mommy, Sleepy Fish",
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=19058",
+      color: ["#588DCE", "#FDFEFC"],
       id: uuidv4(),
       active: false,
     },
-    {
-      name: "Higher",
-      cover: "https://chillhop.com/wp-content/uploads/2021/05/2473c60e471fe9b40e246bf7711c87d3d6ea69a7-1024x1024.jpg",
-      artist: "Misha, Jussi Halme",
-      audio: "https://mp3.chillhop.com/serve.php/?mp3=15088",
-      color: ["#EA7A76", "#2E385B"],
-      id: uuidv4(),
-      active: false,
-    },
+    
   ];
 }
 
