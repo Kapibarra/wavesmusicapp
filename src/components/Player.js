4b124eb0ae57bb2acc8ade09dfb6f6c0df1e71f2
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-06-27 16:35:12 +0300

    refactor

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPlay,
@@ -7,8 +7,8 @@ import {
   faPause,
 } from "@fortawesome/free-solid-svg-icons";
 
-const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
-  const audioRef = useRef(null);
+const Player = ({ currentSong, isPlaying, setIsPlaying, audioRef,setSongInfo,songInfo }) => {
+
 
   const playSongHandler = () => {
     if (isPlaying) {
@@ -19,11 +19,7 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
       setIsPlaying(!isPlaying);
     }
   };
-  const timeUpdateHandler = (e) => {
-    const current = e.target.currentTime;
-    const duration = e.target.duration;
-    setSongInfo({ ...songInfo, currentTime: current, duration: duration });
-  };
+
   const getTime = (time) => {
     return (
       Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
@@ -33,10 +29,7 @@ const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
       audioRef.current.currentTime = e.target.value
 setSongInfo({...songInfo, currentTime : e.target.value})
   }
-  const [songInfo, setSongInfo] = useState({
-    currentTime: 0,
-    duration: 0,
-  });
+
 
   return (
     <div className="player">
@@ -60,12 +53,7 @@ setSongInfo({...songInfo, currentTime : e.target.value})
           size="2x"
         />
       </div>
-      <audio
-        onTimeUpdate={timeUpdateHandler}
-        onLoadedMetadata={timeUpdateHandler}
-        ref={audioRef}
-        src={currentSong.audio}
-      ></audio>
+      
     </div>
   );
 };
