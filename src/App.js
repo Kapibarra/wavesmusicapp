2156e05df1ce3640dd07039f70a8e9bc882510e2
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-06-28 13:37:29 +0300

    nav styles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useRef } from "react";
 import Player from "./components/Player";
 import Song from "./components/Song";
 import Library from "./components/Library";
+import Nav from "./components/Nav"
 import data from "./util";
 import "./styles/app.scss";
 
@@ -16,6 +17,7 @@ function App() {
     currentTime: 0,
     duration: 0,
   });
+  const [libraryStatus, setLibraryStatus] = useState(false)
   const timeUpdateHandler = (e) => {
     const current = e.target.currentTime;
     const duration = e.target.duration;
@@ -23,6 +25,7 @@ function App() {
   };
   return (
     <div className="App">
+    <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus}/>
       <Song currentSong={currentSong} />
       <Player
         audioRef={audioRef}
@@ -38,6 +41,7 @@ function App() {
         audioRef={audioRef}
         isPlaying={isPlaying}
         setSongs={setSongs}
+        libraryStatus={libraryStatus}
       />
       <audio
         onTimeUpdate={timeUpdateHandler}
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,14 +2,16 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMusic } from "@fortawesome/free-solid-svg-icons";
 
-const Nav = () => {
+const Nav = ({setLibraryStatus, libraryStatus}) => {
   return (
     <nav>
       <h1>Waves</h1>
-      <button>
+      <button onClick={() => setLibraryStatus(!libraryStatus)}>
         LibrarySong
         <FontAwesomeIcon icon={faMusic} />
       </button>
     </nav>
   );
 };
+
+export default Nav
