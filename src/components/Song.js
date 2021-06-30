85e8f02c10524f50e8d36ad8909c26159ee3d539
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-06-30 12:59:04 +0300

    added mob fr5i3endly

--- src/components/Song.js
+++ src/components/Song.js
@@ -7,9 +7,6 @@ const Song = ({currentSong}) => {
 <img src={currentSong.cover} alt={currentSong.name}></img>
 <h2>{currentSong.name}</h2>
 <h3>{currentSong.artist}</h3>
-<div id="preloader">
-  <div id="loader"></div>
-</div>
         </div>
 
     )
